3abfa7cd73dbbfccb59b65296900100facde14b9
David Söderberg <granskog_@hotmail.com>
2015-03-17 06:11:43 +0100

    Creating subjects and bodys for notification.

--- app/Repositories/notification/EloquentNotificationRepository.php
+++ app/Repositories/notification/EloquentNotificationRepository.php
@@ -20,25 +20,36 @@ class EloquentNotificationRepository extends CRepository implements Notification
 
 		if(is_numeric($user_id)){
 			$note->user_id = $user_id;
+			$user = $this->user->get($user_id);
+			if(!is_null($user)){
+				$this->user = $user;
+			}else{
+				$this->errors = array('user' => 'That user does not exist.');
+				return false;
+			}
 		}
 
 		if(isset($type) && NotificationType::isType($type)){
 			$note->type = $type;
 		}
 
-		if(isset($subject)){
-			$note->subject;
+		if(isset($subject) && isset($body)){
+			$note->subject = $subject;
+			$note->body = $body;
+		}else{
+			if(isset($type) && NotificationType::isType($type)) {
+				$note = $this->setSubjectAndBody($note, $object);
+			}
 		}
 
-		if(isset($body)){
-			$note->body;
-		}
 
-		$namespaces = explode('\\', get_class($object));
-		$object_type = $namespaces[count($namespaces)-1];
-		if(is_object($object) && class_exists('App\\'.$object_type)){
-			$note->object_id = $object->id;
-			$note->object_type = $object_type;
+		if(is_object($object)){
+			$namespaces = explode('\\', get_class($object));
+			$object_type = $namespaces[count($namespaces)-1];
+			if(class_exists('App\\'.$object_type)) {
+				$note->object_id = $object->id;
+				$note->object_type = $object_type;
+			}
 		}
 
 		$note->sent_at = new \DateTime('now');
@@ -52,24 +63,48 @@ class EloquentNotificationRepository extends CRepository implements Notification
 		}
 	}
 
-	private function sendNotification($notification){
-		if($notification->body != ''){
-			$body = $notification->body;
-		}else{
-			switch($notification->type){
-				default:
-					return false;
-					break;
-			}
+	private function setSubjectAndBody($notification, $object){
+		$from = Auth::user();
+		$notification->subject = 'New '.$notification->type;
+		switch($notification->type){
+			case NotificationType::MENTION:
+				$notification->body = 'You have been mention by '.$from->username. ' in a '.strtolower($notification->object_type).' on this ';
+				if($notification->object_type == 'Reply') {
+					$notification->body .= 'topic.';
+				}
+				if($notification->object_type == 'Comment') {
+					$notification->body .= 'codeblock.';
+				}
+				break;
+			case NotificationType::COMMENT:
+				$notification->body = 'You have a new comment on this codeblock.';
+				break;
+			case NotificationType::FAVOURITE:
+				$notification->subject = 'Your codeblock has been '.$notification->type.'d';
+				$notification->body = $from->username.' has favourited your codeblock.';
+				break;
+			case NotificationType::REPLY:
+				$notification->body = 'You have a new reply on this topic.';
+				break;
+			case NotificationType::ROLE:
+				$notification->body = $from->role->name.' have given you a role as: '.$this->user->role->name;
+				break;
+			case NotificationType::STAR:
+				$notification->body = $from->username.' has added a star to your codeblock.';
+				break;
+			case NotificationType::BANNED:
+				$notification->subject = 'You have been '.$notification->type;
+				$notification->body = $from->role->name.' has '.$notification->type. ' you for some reason, please reply to this email for questions.';
+				break;
 		}
+		return $notification;
+	}
 
-		$user = $this->user->get($notification->user_id);
-		if(!is_null($user)) {
-			$data = array('subject' => $notification->subject, 'body' => $body);
-			$emailInfo = array('toEmail' => $user->email, 'toName' => $user->username, 'subject' => $notification->subject);
-			if($this->sendEmail('emails.notification', $emailInfo, $data) == 1) {
-				return true;
-			}
+	private function sendNotification($notification){
+		$data = array('subject' => $notification->subject, 'body' => $notification->body);
+		$emailInfo = array('toEmail' => $this->user->email, 'toName' => $this->user->username, 'subject' => $notification->subject);
+		if($this->sendEmail('emails.notification', $emailInfo, $data) == 1) {
+			return true;
 		}
 		return false;
 	}
