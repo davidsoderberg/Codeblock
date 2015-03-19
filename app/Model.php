252dc72f1ae3c79c25f84c06224b8970f0922529
David Söderberg <granskog_@hotmail.com>
2015-03-19 19:02:20 +0100

    Adding slug creator.

--- app/Model.php
+++ app/Model.php
@@ -1,6 +1,7 @@
 <?php namespace App;
 
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class Model extends \Illuminate\Database\Eloquent\Model {
 
@@ -13,6 +14,13 @@ class Model extends \Illuminate\Database\Eloquent\Model {
 		static::saving(function($object){
 			return $object::isValid($object);
 		});
+	}
+
+	// From: https://laracasts.com/discuss/channels/general-discussion/how-to-validate-a-slug-unique-in-laravel-5
+	function getSlug($value, $column = 'slug') {
+		$slug = Str::slug($value);
+		$slugCount = count($this->whereRaw($column." REGEXP '^{$slug}(-[0-9]+)?$' and id != '{$this->id}'")->get());
+		return ($slugCount > 0) ? "{$slug}-{$slugCount}" : $slug;
 	}
 
 	// validerings metoden som kör rule variablen från alla modeller när det modell objektet sparas.
