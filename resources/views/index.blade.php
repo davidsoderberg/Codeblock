5e682825e905959501bf84f6485ee60cd1b8444e
David Söderberg <granskog_@hotmail.com>
2015-03-25 19:41:50 +0100

    Fixed typo on homepage.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
 <h2>Welcome</h2>
 <textarea class="code-editor readonly" data-lang="html" id="blockCode" name="code">
 &lt;p&gt;Welcome to codeblock.se, a website where you can share your codeblocks that you use over and over again.&lt;/p&gt;
-&lt;p&gt;Browse others code block or sign up and add your own or comment on another codeblocks.&lt;/p&gt;
+&lt;p&gt;Browse others codeblock or sign up and add your own or comment on another codeblocks.&lt;/p&gt;
 &lt;p&gt;
 	&lt;strong&gt;
 		Welcome/&lt;br&gt;
