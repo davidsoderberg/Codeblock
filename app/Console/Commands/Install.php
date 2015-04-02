14087eb0189715687251d283b6c27ec7acd2b3fc
David Söderberg <granskog_@hotmail.com>
2015-04-02 06:09:39 +0200

    Creating an install console command.

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,133 @@
+<?php namespace App\Console\Commands;
+
+use App\Repositories\Permission\PermissionRepository;
+use App\Repositories\Role\RoleRepository;
+use Illuminate\Support\Facades\DB;
+use App\Repositories\CRepository;
+use App\Services\Github;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class Install extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'Install';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Installs codeblock.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire(RoleRepository $roleRepository, PermissionRepository $permissionRepository)
+	{
+		$startat = $this->option('startat');
+		if(is_null($startat)){
+			$mail = New CRepository();
+			$data = array('subject' => 'Test mail', 'body' => 'This is a mail to test mail configuration.');
+			$emailInfo = array('toEmail' => env('FROM_ADRESS'), 'toName' => env('FROM_NAME'), 'subject' => 'Test mail');
+			if(!$mail->sendEmail('emails.notification', $emailInfo, $data)) {
+				$this->error('The mail is not configured correctly, please try agian and use option startat with "Mail" as value.');
+			}
+			$startat = 'Github';
+		}
+		if($startat == 'Github') {
+			$github = new Github();
+			if(!$github->isToken(env('GITHUB_TOKEN', null))) {
+				$this->error('The github token is not valid, please try agian and use option startat with "Github" as value.');
+			}
+			$startat = 'Database';
+		}
+		if($startat == 'Database') {
+			try {
+				DB::connection()->getDatabaseName();
+			} catch (\Exception $e){
+				$this->error('The database is not configured correctly, please try agian and use option startat with "Database" as value.');
+			}
+			$startat = 'Migration';
+		}
+		if($startat == 'Migration') {
+			try{
+				Artisan::call('migrate');
+			} catch (\Exception $e){
+				$this->error('The migration could not be done for some reason, please try agian and use option startat with "Migration" as value.');
+			}
+			$startat = 'Seed';
+		}
+		if($startat == 'Seed') {
+			try{
+				Artisan::call('db:seed');
+			} catch (\Exception $e){
+				$this->error('The seed of databe could not be done for some reason, please try agian and use option startat with "Seed" as value.');
+			}
+			$startat = 'Permissions';
+		}
+		if($startat == 'Permissions') {
+			try{
+				Artisan::call('InsertPermissions');
+			} catch (\Exception $e){
+				$this->error('The permissions colud not be created for some reason, please try agian and use option startat with "Permissions" as value.');
+			}
+			$startat = 'Role';
+		}
+		if($startat == 'Role') {
+			$ids = array();
+			foreach($permissionRepository->get() as $permission) {
+				$ids[] = $permission->id;
+			}
+			if(!$roleRepository->createOrUpdate(array('name' => 'Super admin', 'default' => 1))) {
+				$this->error('The role could not be created, please try agian.');
+			}
+			if(!$roleRepository->syncPermissions($roleRepository->role, $ids)) {
+				$roleRepository->delete($roleRepository->role->id);
+				$this->error('The role could get any permissions, please try agian and use option startat with "Role" as value.');
+			}
+		}
+		$this->info('You have now installed codeblock.');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return [
+			['startat', 'start', InputOption::VALUE_OPTIONAL, 'Start step is done.', null]
+		];
+	}
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected $commands = [
 		'App\Console\Commands\Inspire',
-		'App\Console\Commands\InsertPermission'
+		'App\Console\Commands\InsertPermission',
+		'App\Console\Commands\Install'
 	];
 
 	/**
