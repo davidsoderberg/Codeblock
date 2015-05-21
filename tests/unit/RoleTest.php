cd789eb88adf0a53f3d63d57c9c0acbd92088a23
David Söderberg <granskog_@hotmail.com>
2015-05-21 05:55:11 +0200

    Adding unit tests for role.

--- tests/unit/RoleTest.php
+++ tests/unit/RoleTest.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Repositories\Article\EloquentArticleRepository;
+use App\Repositories\Role\EloquentRoleRepository;
 
-class ArticleTest extends UnitCase {
+class RoleTest extends UnitCase {
 
 	public $repo;
 
@@ -10,30 +10,30 @@ class ArticleTest extends UnitCase {
 	{
 		parent::setUp();
 		$this->setUpDb(false);
-		$this->repo = new EloquentArticleRepository();
+		$this->repo = new EloquentRoleRepository();
 	}
 
 	public function testCreateOrUpdate(){
-		$input = ['title' => 'test', 'body' => 'test'];
+		$input = ['name' => 'test'];
 		$this->assertTrue($this->repo->createOrUpdate($input));
-		$this->assertfalse($this->repo->createOrUpdate(['title' => ''],1));
+		$this->assertfalse($this->repo->createOrUpdate(['name' => ''],1));
 		$this->assertTrue(is_object($this->repo->getErrors()));
-		$this->assertTrue($this->repo->createOrUpdate(['title' => 'hej'],1));
-		$this->repo->createOrUpdate(['title' => 'test'],1);
+		$this->assertTrue($this->repo->createOrUpdate(['name' => 'hej'],1));
+		$this->repo->createOrUpdate(['name' => 'test'],1);
 	}
 
 	public function testGet(){
 		$this->assertTrue(is_object($this->repo->get()));
 		$this->assertFalse(is_object($this->repo->get(1)));
 
-		$input = ['title' => 'test', 'body' => 'test'];
+		$input = ['name' => 'test'];
 		$this->assertTrue($this->repo->createOrUpdate($input));
 		$this->assertTrue(is_object($this->repo->get(1)));
 	}
 
 	public function testDelete(){
 		$this->assertFalse($this->repo->delete(1));
-		$input = ['title' => 'test', 'body' => 'test'];
+		$input = ['name' => 'test'];
 		$this->assertTrue($this->repo->createOrUpdate($input));
 		$this->assertTrue($this->repo->delete(1));
 	}
