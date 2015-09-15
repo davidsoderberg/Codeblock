37e86667de487ec9c11eb698666f552d3dccdc22
David Söderberg <granskog_@hotmail.com>
2015-09-15 19:53:03 +0200

    Fixed an url bug in sortlinks view.

--- resources/views/partials/sortlinks.blade.php
+++ resources/views/partials/sortlinks.blade.php
@@ -1,4 +1,4 @@
-<a href="/{{$type}}/{{$name}}/date" class="margin-bottom-half full-width-small float-none button">Date</a>
-<a href="/{{$type}}/{{$name}}/name" class="margin-bottom-half full-width-small float-none button">Name</a>
-<a href="/{{$type}}/{{$name}}/stars" class="margin-bottom-half full-width-small float-none button">Stars</a>
-<a href="/{{$type}}/{{$name}}/comments" class="margin-bottom-half full-width-small float-none button">Comments</a>
+<a href="/{{$type}}/{{urlencode($name)}}/date" class="margin-bottom-half full-width-small float-none button">Date</a>
+<a href="/{{$type}}/{{urlencode($name)}}/name" class="margin-bottom-half full-width-small float-none button">Name</a>
+<a href="/{{$type}}/{{urlencode($name)}}/stars" class="margin-bottom-half full-width-small float-none button">Stars</a>
+<a href="/{{$type}}/{{urlencode($name)}}/comments" class="margin-bottom-half full-width-small float-none button">Comments</a>
