07dc61ab9d2f052aeb0f1774af14743460e4a7d2
David Söderberg <granskog_@hotmail.com>
2015-10-16 06:10:32 +0200

    Added migration for team_user.

---
+++ database/migrations/2015_10_14_052821_create_team_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function(Blueprint $table)
+        {
+            $table->increments( 'id' )->unsigned();
+            $table->integer( 'user_id' );
+            $table->integer( 'team_id' );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('team_user');
+    }
+}
