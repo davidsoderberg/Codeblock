4775374e70c799b260d2621e47a0075dbadd4e84
David Söderberg <granskog_@hotmail.com>
2015-11-06 18:15:36 +0100

    Renamed CommentTest to StarTest in StarTest.php.

--- tests/unit/StarTest.php
+++ tests/unit/StarTest.php
@@ -2,7 +2,7 @@
 
 use App\Repositories\Star\EloquentStarRepository;
 
-class CommentTest extends UnitCase {
+class StarTest extends UnitCase {
 
 	public $repo;
 
@@ -13,7 +13,7 @@ class CommentTest extends UnitCase {
 		$this->repo = new EloquentStarRepository();
 	}
 
-	public function testGetComment(){
+	public function testGetStars(){
 		$this->assertTrue(count($this->repo->get()) == 0);
 
 		$input = ['post_id' => 1, 'user_id' => 1];
